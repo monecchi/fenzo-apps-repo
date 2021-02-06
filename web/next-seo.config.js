5a206a33ea9be0579fb69b2697a3ef9693761ab9
Adriano Monecchi <fornacciari@gmail.com>
2021-02-06 18:57:16 -0300

    Create next-seo.config.js

---
+++ web/next-seo.config.js
@@ -0,0 +1,30 @@
+const title = "Fenzo Serralheria";
+const description = "Alumínio alto padrão. Portões, portas, janelas, guarda corpo, vidro temperado";
+
+const SEO = {
+  title,
+  description,
+  canonical: "https://fenzoserralheria.com.br",
+  openGraph: {
+    type: "website",
+    locale: "pt_BR",
+    url: "https://fenzoserralheria.com.br",
+    title,
+    description,
+    images: [
+      {
+        url: "/fenzo-banner.jpg",
+        alt: title,
+        width: 2240,
+        height: 1260,
+      },
+    ],
+  },
+  twitter: {
+    handle: "@fenzoserralheria",
+    site: "@fenzoserralheria",
+    cardType: "summary_large_image",
+  },
+};
+
+export default SEO;
