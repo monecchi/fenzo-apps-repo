ff590c2784b7d46c4391738c0375446a12e429c7
Adriano Monecchi <fornacciari@gmail.com>
2021-04-14 12:33:24 -0300

    Fixed Icon Component Parameters

--- web/src/components/Icon/index.tsx
+++ web/src/components/Icon/index.tsx
@@ -5,23 +5,24 @@ import IcomoonReact from 'icomoon-react' // iconmoon-react
 // import own icons selection.json
 import iconSet from './selection.json'
 
-const Icon = ({
-  color,
-  size,
-  icon
-}) => {
+const Icon: React.FC<{
+  color?: string,
+  size: string | number,
+  icon: string,
+  className?: string,
+  style?: React.CSSProperties
+}> = props => {
+  const { color, size = "100%", icon, className = "", style } = props;
   return (
     <IcomoonReact
       iconSet={iconSet}
       color={color}
-      size={size.toString()}
+      size={size}
       icon={icon}
+      className={className}
+      style={style}
     />
-  );
-}
-
-Icon.defaultProps = {
-  color: "currentColor"
+  )
 }
 
 export default Icon;
